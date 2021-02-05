a7d2bd6c70885d8e834dac58892076a776b95278
genady <jobgenady@gmail.com>
2021-02-05 04:20:15 +0200

    JsonController experiments

--- src/Controllers/JsonController.php
+++ src/Controllers/JsonController.php
@@ -22,18 +22,29 @@ class JsonController
 
     public function getByid(int $post_id){
         $res = $this->post_model->searchById($post_id);
+        if(is_array($res)){
+            $res =  $this->decode($res);
+        }
         $json = json_encode($res, JSON_PRETTY_PRINT);
         ob_start();
         require_once 'public/templates/json_show.php';
         echo ob_get_clean();
     }
     public function getByUserId(int $user_id){
-        var_dump($user_id);
         $res = $this->post_model->searchByUserId($user_id);
+        if(is_array($res)){
+            $res =  $this->decode($res);
+        }
         $json = json_encode($res, JSON_PRETTY_PRINT);
         ob_start();
         require_once 'public/templates/json_show.php';
         echo ob_get_clean();
+    }
+    private function decode(array $res_arr):array{
+        foreach ($res_arr as $key => $post){
+            $res_arr[$key]['body'] = str_replace("\n","",$post['body']);
+        }
+        return $res_arr;
     }
 
 }
